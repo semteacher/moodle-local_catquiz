847e058386acdbfc1a7234c15433ddcd4d9e5e37
David Szkiba <david.szkiba@wunderbyte.at>
2024-06-17 11:27:15 +0200

    Fix lint error

--- export_shortcode_csv.php
+++ export_shortcode_csv.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Based on <https://gist.github.com/kralo/293dbc07b9b318eabe43>.
+// Based on https://gist.github.com/kralo/293dbc07b9b318eabe43.
 
 /**
  * Attempts CSV export
